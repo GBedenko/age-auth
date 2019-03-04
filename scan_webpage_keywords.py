a99303f069cd4949f24a23b5176b5fdbbaef1136
Genaro Bedenko (bedenkog) <bedenkog@coventry.ac.uk>
2019-03-04 21:15:59 +0000

    Commenting to explain webpage scan method

--- scan_webpage_keywords.py
+++ scan_webpage_keywords.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python
 
+# Imports for http requests and regular expressions libraries
 import requests
 import re as regex
 
 def contains_keyword(regex_string, webpage_url):
+    """This class docstring shows how to use sphinx and rst syntax
 
+        :param regex_string: Regular Expression string to run over the webpage url's content
+        :param webpage_url: URL for the webpage to scan, must include http:// or https:// prefix"""
+
+    # GET request on the provided webpage url
     request = requests.get(webpage_url)
 
+    # Save just the response body
     request_html = request.content
 
+    # Execute the regex on the webpage response body
     regex_result = regex.search(regex_string, request_html)
 
+    # If the regex is in the content, return true
     if(regex_result):
         return True
 
+    # Return false if regex didn't find keywords
     return False
